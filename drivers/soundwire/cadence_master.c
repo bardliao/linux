39e1353c6c09557358de0981f850a558b23d9230
Bard Liao <yung-chuan.liao@linux.intel.com>
2019-07-29 15:04:11 +0800

    soundwire: cadence_master: Handle interrupt in a thread
    New interrupt may happen while we are handling current interrupt.
    So handle it by a thread and keep checking if any interrupt remaining.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- drivers/soundwire/cadence_master.c
+++ drivers/soundwire/cadence_master.c
@@ -17,6 +17,7 @@
 #include <linux/soundwire/sdw.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
+#include <linux/workqueue.h>
 #include "bus.h"
 #include "cadence_master.h"
 
@@ -989,7 +990,6 @@ irqreturn_t sdw_cdns_irq(int irq, void *dev_id)
 {
 	struct sdw_cdns *cdns = dev_id;
 	u32 int_status;
-	int ret = IRQ_HANDLED;
 
 	/* Check if the link is up */
 	if (!cdns->link_up)
@@ -997,8 +997,28 @@ irqreturn_t sdw_cdns_irq(int irq, void *dev_id)
 
 	int_status = cdns_readl(cdns, CDNS_MCP_INTSTAT);
 
+	if (int_status & CDNS_MCP_INT_IRQ)
+		return IRQ_WAKE_THREAD;
+
+	return IRQ_NONE;
+}
+EXPORT_SYMBOL(sdw_cdns_irq);
+
+/**
+ * sdw_cdns_thread() - Cadence irq thread handler
+ * @irq: irq number
+ * @dev_id: irq context
+ */
+irqreturn_t sdw_cdns_thread(int irq, void *dev_id)
+{
+	struct sdw_cdns *cdns = dev_id;
+	u32 int_status;
+
+repeat:
+	int_status = cdns_readl(cdns, CDNS_MCP_INTSTAT);
+
 	if (!(int_status & CDNS_MCP_INT_IRQ))
-		return IRQ_NONE;
+		return IRQ_HANDLED;
 
 	if (int_status & CDNS_MCP_INT_RX_WL) {
 		cdns_read_response(cdns);
@@ -1037,22 +1057,21 @@ irqreturn_t sdw_cdns_irq(int irq, void *dev_id)
 			     CDNS_MCP_INT_SLAVE_MASK, 0);
 
 		int_status &= ~CDNS_MCP_INT_SLAVE_MASK;
-		ret = IRQ_WAKE_THREAD;
+		queue_delayed_work(system_power_efficient_wq,
+				   &cdns->work,
+				   msecs_to_jiffies(2));
 	}
 
 	cdns_writel(cdns, CDNS_MCP_INTSTAT, int_status);
-	return ret;
-}
-EXPORT_SYMBOL(sdw_cdns_irq);
 
-/**
- * sdw_cdns_thread() - Cadence irq thread handler
- * @irq: irq number
- * @dev_id: irq context
- */
-irqreturn_t sdw_cdns_thread(int irq, void *dev_id)
+	goto repeat;
+}
+EXPORT_SYMBOL(sdw_cdns_thread);
+
+static void cdns_update_slave_status_work(struct work_struct *work)
 {
-	struct sdw_cdns *cdns = dev_id;
+	struct sdw_cdns *cdns =
+		container_of(work, struct sdw_cdns, work.work);
 	u32 slave0, slave1;
 
 	dev_dbg_ratelimited(cdns->dev, "Slave status change\n");
@@ -1068,10 +1087,7 @@ irqreturn_t sdw_cdns_thread(int irq, void *dev_id)
 	cdns_writel(cdns, CDNS_MCP_INTSTAT, CDNS_MCP_INT_SLAVE_MASK);
 	cdns_updatel(cdns, CDNS_MCP_INTMASK,
 		     CDNS_MCP_INT_SLAVE_MASK, CDNS_MCP_INT_SLAVE_MASK);
-
-	return IRQ_HANDLED;
 }
-EXPORT_SYMBOL(sdw_cdns_thread);
 
 /*
  * init routines
@@ -1140,6 +1156,7 @@ int sdw_cdns_enable_interrupt(struct sdw_cdns *cdns, bool state)
 
 		if (interrupt_mask) /* parameter override */
 			mask = interrupt_mask;
+
 	} else {
 		cdns_writel(cdns, CDNS_MCP_SLAVE_INTMASK0, 0);
 		cdns_writel(cdns, CDNS_MCP_SLAVE_INTMASK1, 0);
@@ -1500,6 +1517,7 @@ int sdw_cdns_probe(struct sdw_cdns *cdns)
 	init_completion(&cdns->tx_complete);
 	cdns->bus.port_ops = &cdns_port_ops;
 
+	INIT_DELAYED_WORK(&cdns->work, cdns_update_slave_status_work);
 	return 0;
 }
 EXPORT_SYMBOL(sdw_cdns_probe);
