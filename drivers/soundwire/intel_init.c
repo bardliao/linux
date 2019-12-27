32dd7ee5c6883dd2506d3d8b70c967588d21cf05
Bard Liao <yung-chuan.liao@linux.intel.com>
2019-12-27 17:18:14 +0800

    Soundwire: intel_init: save Slave(s) _ADR info in sdw_intel_ctx
    Save ACPI information in context so that we can match machine driver
    with sdw _ADR matching tables.
    
    Suggested-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel_init.c
+++ drivers/soundwire/intel_init.c
@@ -71,6 +71,9 @@ static int sdw_intel_cleanup(struct sdw_intel_ctx *ctx)
 		sdw_intel_master_unregister(link->master_dev);
 	}
 
+	kfree(ctx->ids);
+	ctx->ids = NULL;
+
 	return 0;
 }
 
@@ -183,8 +186,12 @@ static struct sdw_intel_ctx
 	struct sdw_intel_link_res *link;
 	struct sdw_intel_ctx *ctx;
 	struct acpi_device *adev;
-	struct sdw_intel_master_dev *master_dev;
+	struct sdw_master_device *md;
+	struct sdw_slave *slave;
+	struct list_head *node;
+	struct sdw_bus *bus;
 	u32 link_mask;
+	int num_slaves = 0;
 	int count;
 	int i;
 
@@ -249,6 +256,25 @@ static struct sdw_intel_ctx
 		link->md = md;
 
 		list_add_tail(&link->list, &ctx->link_list);
+		bus = &link->cdns->bus;
+		/* Calculate number of slaves */
+		list_for_each(node, &bus->slaves)
+			num_slaves++;
+	}
+
+	ctx->ids = kcalloc(num_slaves, sizeof(*ctx->ids), GFP_KERNEL);
+	if (!ctx->ids)
+		goto err;
+
+	ctx->num_slaves = num_slaves;
+	i = 0;
+	list_for_each_entry(link, &ctx->link_list, list) {
+		bus = &link->cdns->bus;
+		list_for_each_entry(slave, &bus->slaves, node) {
+			ctx->ids[i].id = slave->id;
+			ctx->ids[i].link_id = bus->link_id;
+			i++;
+		}
 	}
 
 	return ctx;
