1dd633a5d0b2a36f44e728eb91c7ec1c68997893
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-09-09 17:52:40 -0500

    fixup! soundwire: bus: add enumerated slave to device list
    On ACPI platforms we need to discard dev.of_node, otherwise the probe
    fails.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/bus_type.c
+++ drivers/soundwire/bus_type.c
@@ -87,7 +87,10 @@ static int sdw_drv_probe(struct device *dev)
 	/*
 	 * fw description is mandatory to bind
 	 */
-	if (!dev->fwnode || !dev->of_node)
+	if (!dev->fwnode)
+		return -ENODEV;
+
+	if (!IS_ENABLED(CONFIG_ACPI) && !dev->of_node)
 		return -ENODEV;
 
 	id = sdw_get_device_id(slave, drv);
