0fd0df7204c5f772c7b3127241d5f21ff57b5a78
Bard Liao <yung-chuan.liao@linux.intel.com>
2024-04-30 08:42:54 +0800

    ASoC: Intel: soc-acpi-intel-lnl-match: add gen6 AIOC mach
    rt712-vb on link 2 and rt1320 on link 1
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- sound/soc/intel/common/soc-acpi-intel-lnl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-lnl-match.c
@@ -46,8 +46,8 @@ static const struct snd_soc_acpi_endpoint rt712_endpoints[] = {
 	{
 		.num = 1,
 		.aggregated = 0,
-		.group_position = 0,
-		.group_id = 0,
+		.group_position = 2,
+		.group_id = 1,
 	},
 };
 
@@ -184,6 +184,15 @@ static const struct snd_soc_acpi_adr_device rt1318_2_group1_adr[] = {
 	}
 };
 
+static const struct snd_soc_acpi_adr_device rt1320_1_single_adr[] = {
+	{
+		.adr = 0x000130025D132000ull,
+		.num_endpoints = 1,
+		.endpoints = &spk_l_endpoint,
+		.name_prefix = "rt1320"
+	}
+};
+
 static const struct snd_soc_acpi_adr_device rt714_0_adr[] = {
 	{
 		.adr = 0x000030025D071401ull,
@@ -229,6 +238,20 @@ static const struct snd_soc_acpi_link_adr lnl_712_only[] = {
 		.mask = BIT(3),
 		.num_adr = ARRAY_SIZE(rt1712_3_single_adr),
 		.adr_d = rt1712_3_single_adr,
+	},
+	{}
+};
+
+static const struct snd_soc_acpi_link_adr lnl_712_l2_1320_l1[] = {
+	{
+		.mask = BIT(2),
+		.num_adr = ARRAY_SIZE(rt712_2_single_adr),
+		.adr_d = rt712_2_single_adr,
+	},
+	{
+		.mask = BIT(1),
+		.num_adr = ARRAY_SIZE(rt1320_1_single_adr),
+		.adr_d = rt1320_1_single_adr,
 	},
 	{}
 };
@@ -329,6 +352,12 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_lnl_sdw_machines[] = {
 		.links = lnl_712_only,
 		.drv_name = "sof_sdw",
 		.sof_tplg_filename = "sof-lnl-rt712-l2-rt1712-l3.tplg",
+	},
+	{
+		.link_mask = BIT(2) | BIT(1),
+		.links = lnl_712_l2_1320_l1,
+		.drv_name = "sof_sdw",
+		.sof_tplg_filename = "sof-lnl-rt712-l2-rt1320-l1.tplg",
 	},
 	{
 		.link_mask = BIT(0),
