a600d17f7c5aef75bbd197aa62cd571fef574b60
Bard Liao <yung-chuan.liao@linux.intel.com>
2025-02-07 15:08:57 +0800

    ASoC: SOF: revise snd_sof_find_spcm_dai
    snd_sof_find_spcm_dai() is looking for the spcm that matches the
    rtd->dai_link->id. And the FE DAI link id is pcm->pcm_id instead of
    pcm->dai_id.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- sound/soc/sof/sof-audio.h
+++ sound/soc/sof/sof-audio.h
@@ -602,7 +602,7 @@ struct snd_sof_pcm *snd_sof_find_spcm_dai(struct snd_soc_component *scomp,
 	struct snd_sof_pcm *spcm;
 
 	list_for_each_entry(spcm, &sdev->pcm_list, list) {
-		if (le32_to_cpu(spcm->pcm.dai_id) == rtd->dai_link->id)
+		if (le32_to_cpu(spcm->pcm.pcm_id) == rtd->dai_link->id)
 			return spcm;
 	}
 
