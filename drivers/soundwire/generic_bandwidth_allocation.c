92218520d8cb90b87dd8f966a011dd5e46bb5fb3
Bard Liao <yung-chuan.liao@linux.intel.com>
2025-04-28 18:05:31 +0800

    soundwire: only compute port params in specific stream states
    Currently, sdw_compute_master_ports() is blindly called for every single
    Manager runtime. However, we should not take in count the stream's
    bandwidth if the stream is just allocated or already deprepared.
    
    Fixes: 25befdf32aa4 ("soundwire: generic_bandwidth_allocation: count the bandwidth of active streams only")
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- drivers/soundwire/generic_bandwidth_allocation.c
+++ drivers/soundwire/generic_bandwidth_allocation.c
@@ -206,6 +206,15 @@ static void _sdw_compute_port_params(struct sdw_bus *bus,
 			port_bo = 1;
 
 			list_for_each_entry(m_rt, &bus->m_rt_list, bus_node) {
+				/*
+				 * Only runtimes with CONFIGURED, PREPARED, ENABLED, and DISABLED
+				 * states should be included in the bandwidth calculation.
+				 */
+				if (m_rt->stream->state != SDW_STREAM_CONFIGURED &&
+				    m_rt->stream->state != SDW_STREAM_PREPARED &&
+				    m_rt->stream->state != SDW_STREAM_ENABLED &&
+				    m_rt->stream->state != SDW_STREAM_DISABLED)
+					continue;
 				sdw_compute_master_ports(m_rt, &params[i], &port_bo, hstop);
 			}
 
