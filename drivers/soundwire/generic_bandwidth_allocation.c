9501c91f42f6d2e016c07f3412d16af1ca99d976
Bard Liao <yung-chuan.liao@linux.intel.com>
2025-11-07 13:55:48 +0800

    soundwire: generic_bandwidth_allocation: check bandwidth with real colume
    The existing code assumes the column number will not change, but it
    could change if curr_dr_freq changes. Calculate the new column number
    before checking the bandwidth to make the checking be more accurate.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- drivers/soundwire/generic_bandwidth_allocation.c
+++ drivers/soundwire/generic_bandwidth_allocation.c
@@ -584,7 +584,12 @@ static int sdw_compute_bus_params(struct sdw_bus *bus)
 	if (!is_clock_scaling_supported(bus) || bus->bpt_stream_refcount)
 		clk_values = 1;
 
+	if (!mstr_prop->default_frame_rate || !mstr_prop->default_row)
+		return -EINVAL;
+
 	for (i = 0; i < clk_values; i++) {
+		int total_col;
+
 		if (!clk_buf)
 			curr_dr_freq = bus->params.max_dr_freq;
 		else
@@ -596,8 +601,10 @@ static int sdw_compute_bus_params(struct sdw_bus *bus)
 		if (bus->bpt_stream_refcount)
 			curr_dr_freq = bus->params.max_dr_freq;
 
-		if (curr_dr_freq * (mstr_prop->default_col - 1) >=
-		    bus->params.bandwidth * mstr_prop->default_col)
+		total_col = curr_dr_freq / mstr_prop->default_frame_rate / mstr_prop->default_row;
+
+		if (curr_dr_freq * (total_col - 1) >=
+		    bus->params.bandwidth * total_col)
 			break;
 
 		list_for_each_entry(m_rt, &bus->m_rt_list, bus_node) {
@@ -658,9 +665,6 @@ out:
 			m_p_rt->lane = m_lane;
 		}
 	}
-
-	if (!mstr_prop->default_frame_rate || !mstr_prop->default_row)
-		return -EINVAL;
 
 	mstr_prop->default_col = curr_dr_freq / mstr_prop->default_frame_rate /
 				 mstr_prop->default_row;
