62bc67f12ab47af88976727132b188bc69b32a1e
Simon Trimmer <simont@opensource.cirrus.com>
2026-02-10 18:15:06 +0000

    DEBUG soundwire: bus: wrap BPT in a mutex
    To ensure that only one BPT client can be active at a time
    
    Change-Id: I7330828a926c5779e2d97abd8603ce5756bd5c7b
    Signed-off-by: Simon Trimmer <simont@opensource.cirrus.com>

--- include/linux/soundwire/sdw.h
+++ include/linux/soundwire/sdw.h
@@ -1062,6 +1062,7 @@ struct sdw_bus {
 	int stream_refcount;
 	int bpt_stream_refcount;
 	struct sdw_stream_runtime *bpt_stream;
+	struct mutex bpt_lock;
 	const struct sdw_master_ops *ops;
 	const struct sdw_master_port_ops *port_ops;
 	struct sdw_master_prop prop;
